74c51edfff747b34da4da552da7213bc16f6c67b
Manuel Andres Mendieta <46815715+manuelmendieta21@users.noreply.github.com>
2023-12-12 15:26:28 -0500

    Implicit and explicit
    Agrega el archivo de ejemplos de forma implicita y explicita js
    
    Co-Authored-By: Manuel Andres Mendieta <46815715+manuelmendieta21@users.noreply.github.com>

--- clase-null-undefined.js
+++ clase-null-undefined.js
@@ -1,1 +1,10 @@
 //Create class undefine
+
+//BigInt numero grandes numeros matematicos.
+const bigNumber = 1092922992929292929292929292929n
+console.log(bigNumber)
+
+const numberOfParricles=1000000000000000000000000000000000000000000n
+console.log(numberOfParricles)
+
+
