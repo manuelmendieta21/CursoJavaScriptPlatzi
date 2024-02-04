e29d29f6ba578b92d8708277312dd097bb757286
Manuel Andres Mendieta <46815715+manuelmendieta21@users.noreply.github.com>
2024-02-04 10:19:25 -0500

    changes commit

---
+++ 09-break-continue.js
@@ -0,0 +1,7 @@
+let i=0;
+while (i<6){
+    i++;
+    if(i===2)
+    {continue; }
+    console.log(i); //ignora o pula a iteração
+}
---
+++ 10-switch.js
@@ -0,0 +1,12 @@
+let accion = 'guaaardar';
+
+switch(accion){
+    case 'listar':
+        console.log('accion de listar');
+        break;
+    case 'guardar':
+        console.log('Accion de guardar');
+        break;    
+    default:
+        console.log('No se reconoce la acción');
+}
